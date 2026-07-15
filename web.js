168f92a533e1926a3ef4568b0263a1cb60f3e7c0
2k11dentusapa <nguyentruongthaogiang@gmail.com>
2026-07-15 20:01:06 +0700

    Update web.js

--- web.js
+++ web.js
@@ -1,45 +1,34 @@
-const getIdsElement = document.getElementById, getClassElements = document.getElementsByClassName;
+const $ = k => document.getElementById(k), $$ = j => document.getElementsByClassName(j);
 setInterval(() => {
-    const timeRemaining = (new Date("2027-02-06 00:00:00") - (new Date());
-    if (timeRemaining) {
-        const ngay = Math.floor(timeRemaining / (1000 * 60 * 60 * 24)),
-              gio = Math.floor(timeRemaining / (1000 * 60 * 60) % 24),
-              phut = Math.floor(timeRemaining / (1000 * 60) % 60),
-              giay = Math.floor(timeRemaining / (1000) % 60),
-              f = [ngay, gio, phut, giay];
-        for ( let i = 1; i < 5; i++) {
-            getIdsElement(`Js${i}`).innerHTML = f[i];
-        }
-    } else {
-        for (let i = 1; i < 5; i++) {
-            getIdsElement(`Js${i}`).innerHTML = 0;
-        }
-    }
+    const remn = new Date("2027-02-06 00:00:00") - new Date();
+    if (remn) {
+        const f = [Math.floor(remn/(1000*60*60*24)),Math.floor(remn/(1000*60*60)%24),Math.floor(remn/(1000*60)%60),Math.floor(remn/(1000)%60)];
+        for ( let i = 1; i < 5; i++) $(`Js${i}`).innerHTML = f[i-1];
+    } else for (let i = 1; i < 5; i++) $(`Js${i}`).innerHTML = 0
 }, 1000);
- getClassElements('csscount').forEach(h => {
+ $$('csscount').forEach(h => {
      const g = h.style;
      h.addEventListener("mouseover", function() {
          g.transform = "rotate(3deg) scale(1.04)";
          g.filter = "brightness(1.01)";
          g.transition = "transform 0.35s";
-         g.boxShadow = "0 12px 24px rgba(0, 0, 0, 0.32)";
-
+         g.boxShadow = "0 12px 24px rgba(0, 0, 0, 0.32)"
      });
      h.addEventListener("mouseout", function() {
          g.transform = "rotate(0deg) scale(1.00)";
          g.filter = "brightness(1.00)";
-         g.boxShadow = "0 8px 16px rgba(0, 0, 0, 0.3)";
+         g.boxShadow = "0 8px 16px rgba(0, 0, 0, 0.3)"
      });
  });
- getClassElements('.btn').forEach(n => {
+ $$('.btn').forEach(n => {
      const g = n.style;
      n.addEventListener("mouseover", function() {
          g.transform = "scale(1.07)";
          g.filter = "brightness(1.1)";
-         g.transition = "transform 0.35s";
+         g.transition = "transform 0.35s"
      });
      n.addEventListener("mouseout", function() {
          g.transform = "rotate(0deg) scale(1.00)";
          g.filter = "brightness(1.05)";
-     });
+     })
  });
